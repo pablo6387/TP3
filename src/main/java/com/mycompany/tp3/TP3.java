4a2534ed0bab36644f0bf1e17b2f9fdd329723d7
pablo6387 <56855366+pablo6387@users.noreply.github.com>
2023-08-20 19:27:09 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/tp3/TP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp3;
+
+/**
+ *
+ * @author Pablo
+ */
+public class TP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
